491019d38bce64b5182a4243f3d9dc1248c3d888
ghrahul <ghrahul2016@gmail.com>
2018-12-26 06:13:18 +0000

    Edit functionality of article is added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,10 @@ class ArticlesController < ApplicationController
     
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def create
     #render plain: params[:article].inspect #to check what is passing
     @article = Article.new(article_params)
@@ -13,6 +17,16 @@ class ArticlesController < ApplicationController
       redirect_to article_path(@article)
     else
       render 'new' #again same template
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
   
