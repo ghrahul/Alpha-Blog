e4c21d6f0f6d1bfdabf57aeee9956e3cc71dbf91
ghrahul <ghrahul2016@gmail.com>
2019-01-08 16:39:36 +0000

    Article category many to many association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user #user singular because one-to-many
- 
+  has_many :article_categories #many that's why article categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true, length: { minimum: 3, maximum: 50}
   validates :description, presence: true, length: { minimum: 10, maximum: 300}
   validates :user_id, presence: true
